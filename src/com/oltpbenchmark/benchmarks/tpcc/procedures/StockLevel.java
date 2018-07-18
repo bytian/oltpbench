eb2819975c6a052f0c25c06315657124f95d22eb
Boyu Tian <bytian@umich.edu>
2018-07-18 14:01:58 -0400

    StockLevel

--- src/com/oltpbenchmark/benchmarks/tpcc/procedures/StockLevel.java
+++ src/com/oltpbenchmark/benchmarks/tpcc/procedures/StockLevel.java
@@ -35,23 +35,23 @@ public class StockLevel extends TPCCProcedure {
 
 //    private static final Logger LOG = Logger.getLogger(StockLevel.class);
 //
-//	public SQLStmt stockGetDistOrderIdSQL = new SQLStmt(
-//	        "SELECT D_NEXT_O_ID " +
-//            "  FROM " + TPCCConstants.TABLENAME_DISTRICT +
-//	        " WHERE D_W_ID = ? " +
-//            "   AND D_ID = ?");
-//
-//	public SQLStmt stockGetCountStockSQL = new SQLStmt(
-//	        "SELECT COUNT(DISTINCT (S_I_ID)) AS STOCK_COUNT " +
-//			" FROM " + TPCCConstants.TABLENAME_ORDERLINE + ", " + TPCCConstants.TABLENAME_STOCK +
-//			" WHERE OL_W_ID = ?" +
-//			" AND OL_D_ID = ?" +
-//			" AND OL_O_ID < ?" +
-//			" AND OL_O_ID >= ?" +
-//			" AND S_W_ID = ?" +
-//			" AND S_I_ID = OL_I_ID" +
-//			" AND S_QUANTITY < ?");
-//
+	public SQLStmt stockGetDistOrderIdSQL = new SQLStmt(
+	        "SELECT D_NEXT_O_ID " +
+            "  FROM " + TPCCConstants.TABLENAME_DISTRICT +
+	        " WHERE D_W_ID = ? " +
+            "   AND D_ID = ?");
+
+	public SQLStmt stockGetCountStockSQL = new SQLStmt(
+	        "SELECT COUNT(DISTINCT (S_I_ID)) AS STOCK_COUNT " +
+			" FROM " + TPCCConstants.TABLENAME_ORDERLINE + ", " + TPCCConstants.TABLENAME_STOCK +
+			" WHERE OL_W_ID = ?" +
+			" AND OL_D_ID = ?" +
+			" AND OL_O_ID < ?" +
+			" AND OL_O_ID >= ?" +
+			" AND S_W_ID = ?" +
+			" AND S_I_ID = OL_I_ID" +
+			" AND S_QUANTITY < ?");
+
 //	// Stock Level Txn
 //	private PreparedStatement stockGetDistOrderId = null;
 //	private PreparedStatement stockGetCountStock = null;
@@ -66,8 +66,7 @@ public class StockLevel extends TPCCProcedure {
 //	     stockGetDistOrderId = this.getPreparedStatement(conn, stockGetDistOrderIdSQL);
 //	     stockGetCountStock= this.getPreparedStatement(conn, stockGetCountStockSQL);
 
-         HashSet<String> readSet = new HashSet<String>();
-         HashSet<String> writeSet = new HashSet<String>();
+         String procedureString = "StockLevel Transaction: \n";
 
 	     int threshold = TPCCUtil.randomNumber(10, 20, gen);
 	     int d_id = TPCCUtil.randomNumber(terminalDistrictLowerID,terminalDistrictUpperID, gen);
@@ -76,62 +75,25 @@ public class StockLevel extends TPCCProcedure {
 	     // XXX int i_id = 0;
 	     int stock_count = 0;
 
-         readSet.add("D," + Integer.toString(w_id) + "," +
-                 Integer.toString(d_id));
-         writeSet.add("D," + Integer.toString(w_id) + "," +
-                 Integer.toString(d_id));
+	     procedureString += "\t" +
+                 "SELECT D_NEXT_O_ID " +
+                 "  FROM " + TPCCConstants.TABLENAME_DISTRICT +
+                 " WHERE D_W_ID = " + Integer.toString(w_id) +
+                 "   AND D_ID = " + Integer.toString(d_id) + "\n";
 
-		 TPCCBenchmark.exec.execute(TPCCWorker.StockLevelTrx, readSet, writeSet);
+	     procedureString += "\t" +
+                 "SELECT COUNT(DISTINCT (S_I_ID)) AS STOCK_COUNT " +
+                 " FROM " + TPCCConstants.TABLENAME_ORDERLINE + ", " + TPCCConstants.TABLENAME_STOCK +
+                 " WHERE OL_W_ID = " + Integer.toString(w_id) +
+                 " AND OL_D_ID = " + Integer.toString(d_id) +
+                 " AND OL_O_ID < ?" +
+                 " AND OL_O_ID >= ?" +
+                 " AND S_W_ID = " + Integer.toString(w_id) +
+                 " AND S_I_ID = OL_I_ID" +
+                 " AND S_QUANTITY < " + Integer.toString(threshold) + "\n";
 
+         TPCCBenchmark.exec.log(procedureString);
 
-//	     stockGetDistOrderId.setInt(1, w_id);
-//         stockGetDistOrderId.setInt(2, d_id);
-//         if (trace) LOG.trace(String.format("stockGetDistOrderId BEGIN [W_ID=%d, D_ID=%d]", w_id, d_id));
-//         ResultSet rs = stockGetDistOrderId.executeQuery();
-//         if (trace) LOG.trace("stockGetDistOrderId END");
-//
-//         if (!rs.next()) {
-//             throw new RuntimeException("D_W_ID="+ w_id +" D_ID="+ d_id+" not found!");
-//         }
-//         o_id = rs.getInt("D_NEXT_O_ID");
-//         rs.close();
-//
-//         stockGetCountStock.setInt(1, w_id);
-//         stockGetCountStock.setInt(2, d_id);
-//         stockGetCountStock.setInt(3, o_id);
-//         stockGetCountStock.setInt(4, o_id - 20);
-//         stockGetCountStock.setInt(5, w_id);
-//         stockGetCountStock.setInt(6, threshold);
-//         if (trace) LOG.trace(String.format("stockGetCountStock BEGIN [W_ID=%d, D_ID=%d, O_ID=%d]", w_id, d_id, o_id));
-//         rs = stockGetCountStock.executeQuery();
-//         if (trace) LOG.trace("stockGetCountStock END");
-//
-//         if (!rs.next()) {
-//             String msg = String.format("Failed to get StockLevel result for COUNT query " +
-//                                        "[W_ID=%d, D_ID=%d, O_ID=%d]", w_id, d_id, o_id);
-//             if (trace) LOG.warn(msg);
-//             throw new RuntimeException(msg);
-//         }
-//         stock_count = rs.getInt("STOCK_COUNT");
-//         if (trace) LOG.trace("stockGetCountStock RESULT=" + stock_count);
-//
-//         conn.commit();
-//         rs.close();
-//
-//         if (trace) {
-//             StringBuilder terminalMessage = new StringBuilder();
-//             terminalMessage.append("\n+-------------------------- STOCK-LEVEL --------------------------+");
-//             terminalMessage.append("\n Warehouse: ");
-//             terminalMessage.append(w_id);
-//             terminalMessage.append("\n District:  ");
-//             terminalMessage.append(d_id);
-//             terminalMessage.append("\n\n Stock Level Threshold: ");
-//             terminalMessage.append(threshold);
-//             terminalMessage.append("\n Low Stock Count:       ");
-//             terminalMessage.append(stock_count);
-//             terminalMessage.append("\n+-----------------------------------------------------------------+\n\n");
-//             LOG.trace(terminalMessage.toString());
-//         }
          return null;
 	 }
 }
